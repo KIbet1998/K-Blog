f25032925c9cc8c1201ceb4255a55ed906942d8d
Kibet 1998 <ronoronald1998@gmail.com>
2021-06-20 16:35:46 +0300

    add class config

---
+++ App/config.py
@@ -0,0 +1,25 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:access@localhost/blog'
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
